45aceb9254546c7e7aa15ca9a6e310f442170d8c
maksimvoevodskij81 <61758319+maksimvoevodskij81@users.noreply.github.com>
2020-09-15 17:03:19 +0300

    inition commit add

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(".navbar-collapse a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
+
+$(window).scroll(function () {
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+});
+$(".home-slider").owlCarousel({
+  animateOut: "fadeOut",
+  items: 1,
+  loop: true,
+  dots: false,
+  autoplayHoverPause: false,
+  autoplay: true,
+  smartSpeed: 1000,
+});
