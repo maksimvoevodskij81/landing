54dcab920215a8173d1bfc9727d343d4f2e115be
maksimvoevodskij81 <61758319+maksimvoevodskij81@users.noreply.github.com>
2020-09-26 22:44:51 +0300

    master

--- js/script.js
+++ js/script.js
@@ -18,3 +18,43 @@ $(".home-slider").owlCarousel({
   autoplay: true,
   smartSpeed: 1000,
 });
+
+$(".owl-courses").owlCarousel({
+  animateOut: "fadeOut",
+  loop: true,
+  autoplayHoverPause: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  dots: false,
+  nav: true,
+  navText: [
+    '<i class="fa fa-angle-left"></i>',
+    '<i class="fa fa-angle-right"></i>',
+  ],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".owl-client").owlCarousel({
+  animateOut: "fadeOut",
+  loop: true,
+  autoplayHoverPause: false,
+  autoplay: true,
+  smartSpeed: 1000,
+  removeClass: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
